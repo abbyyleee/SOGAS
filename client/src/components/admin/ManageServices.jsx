e7e92dac5ad0602e256be664036b6aa9155ae720
Abby Lee <abbychrislee@gmail.com>
2025-08-29 17:01:59 -0500

    Service sections added for upcoming functionality

--- client/src/components/admin/ManageServices.jsx
+++ client/src/components/admin/ManageServices.jsx
@@ -10,28 +10,47 @@ const fadeIn = {
 
 export default function ManageServices() {
     return (
-        <motion.div
-            className="min-h-screen bg-deep-blue text-white px-4 py-32"
-            variants={fadeIn}
-            initials="hidden"
-            animate="show"
-        >
-            {/* Back Button */}
-            <div className="mb-6">
+        <div className="min-h-screen bg-deep-blue text-white px-10 py-24 mx-auto">
+            
+            {/* Back To Dashboard */}
+            <div className="mb-8">
                 <Link
                     to="/admin"
-                    className="inline-block px-4 py-2 rounded-xl bg-rust text-dark-navy hover:bg-light-blue transition ring-1 ring-light-blue"
+                    className="inline-block px-4 py-2 rounded-xl bg-rust text-dark-navy hover:bg-soft-blue hover:text-dark-navy transition ring-1 ring-light-blue"
                 >
                     ← Back to Admin Dashboard 
                 </Link>    
             </div>
 
-            {/* Place Holder for management features */}
-            <div className="bg-light-blue/10 border border-light-blue/30 rounded-2xl p-8">
-                <p className="text-white/70 text-lg">
-                    Service management tools will go here.
-                </p>
-            </div>
-        </motion.div>
+            {/* Page Title */}
+            <motion.div
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y:0 }}
+                transition={{ duration: 0.4 }}
+                className="text-3xl font-bold mb-10"
+            >
+                Manage Services 
+            </motion.div>
+
+            {/* Add New Service */}
+            <section className="mb-12">
+                <h2 className="text-xl font-semibold text-white mb-4">Add New Services</h2>
+                
+                {/* Service Form */}
+                <div className="rounded-xl bg-soft-blue/10 p-6 ring-1 ring-light-blue">
+                    <p className="text-white">Form Here</p>
+                </div>
+            </section>
+
+            {/* Current Services */}
+            <section>
+                <h2 className="text-xl font-semibold text-white mb-4">Current Services</h2>
+                
+                {/* List of Services */}
+                <div className="rounded-xl bg-soft-blue/10 p-6 ring-1 ring-light-blue">
+                    <p className="text-white">List will go here.</p>
+                </div>
+            </section>    
+        </div>
     );
 }
