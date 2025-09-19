1cb14bd1a0e4ba81e51930136666b11c70a8fb1f
Abby Lee <abbychrislee@gmail.com>
2025-09-19 14:50:39 -0500

    Minor fix

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://sogas-backend.onrender.com/api",
+    baseURL: "https://sogas-backend.onrender.com/api",
     timeout: 5000,
     headers: {
         "Content-Type": "application/json",
