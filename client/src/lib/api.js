6d6c27e597c886f4f62e6ce3acb3201893b0145b
Abby Lee <abbychrislee@gmail.com>
2025-10-30 14:32:16 -0500

    Fixed typo

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://api.sogasservices.com",
+    baseURL: "https://api.sogasservices.com/api",
     timeout: 5000,
     headers: {
         "Content-Type": "application/json",
