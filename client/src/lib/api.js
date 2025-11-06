8d46fe5235f8e7fe5507f5d6d48385c343a2aa97
Abby Lee <abbychrislee@gmail.com>
2025-11-06 16:43:46 -0600

    Troubleshooting edit service function

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,19 +1,14 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:3000";
-
 const api = axios.create({
-  baseURL: `${API_BASE}/api`,
+  baseURL: import.meta.env.VITE_API_BASE + "/api",
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
 
-// JWT if logged in
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
